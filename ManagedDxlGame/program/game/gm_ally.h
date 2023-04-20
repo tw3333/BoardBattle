518c9d7411d1384f677c6aac20c832b5c5df5e80
twr333 <vthpjn@gmail.com>
2023-04-20 22:05:04 +0900

    a

---
+++ ManagedDxlGame/program/game/gm_ally.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Ally {
+public:
+
+
+
+
+private:
+
+
+};
---
+++ ManagedDxlGame/program/game/gm_unit.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Unit {
+public:
+
+	virtual ~Unit(){}
+
+	enum class Type {
+		Ally,
+		Enemy,
+		Obstacle
+	};
+
+private:
+
+
+};
