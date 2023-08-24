7e5d78bc015a600cb6f669203b4dee95ef12185a
twr333 <vthpjn@gmail.com>
2023-08-24 20:51:40 +0900

    再起動前コミット

--- ManagedDxlGame/program/game/gm_card_range.h
+++ ManagedDxlGame/program/game/gm_card_range.h
@@ -20,7 +20,7 @@ public:
 
 	virtual void DisplayRange(UnitAlly* act_ally, Board* board) = 0;
 	virtual std::vector<Unit*> GetUnitInRange(UnitAlly* act_ally, std::vector<Unit*> all_units) = 0;
-
+	virtual std::vector<SquarePos> GetRangeSquarePos() = 0;
 
 	bool GetIsUnitInRange() { return is_unit_in_range_; }
 
@@ -31,4 +31,5 @@ protected:
 	bool is_unit_in_range_ = false;
 
 
+	std::vector<SquarePos> range_square_pos_;
 };
