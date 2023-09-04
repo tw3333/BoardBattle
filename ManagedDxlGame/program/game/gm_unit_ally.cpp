594e9e963310bae474efded3c749860db20be916
hyou0064 <vthpjn@gmail.com>
2023-09-04 16:11:31 +0900

    komitto

--- ManagedDxlGame/program/game/gm_unit_ally.cpp
+++ ManagedDxlGame/program/game/gm_unit_ally.cpp
@@ -115,18 +115,15 @@ void UnitAlly::ShuffleUseDeck() {
 
 void UnitAlly::Update(float delta_time) {
 
+	//ObjUpdate(Œã‚ÅØ‚è•ª‚¯)
 	int w1 = ((DXE_WINDOW_WIDTH / 10) * 8) /10;
 	int h1 = ((DXE_WINDOW_HEIGHT / 10) * 8) / 10;
-
 	int board_w = (DXE_WINDOW_WIDTH / 10) * 8;
 	int board_h = (DXE_WINDOW_HEIGHT / 10) * 8;
-
-
 	obj_->pos_.z = board_h - (h1 / 2) - (h1) * unit_square_pos_.row;
 	obj_->pos_.x = (w1 / 2) + ((w1) * unit_square_pos_.col);
-
 	unit_obj_->pos_.z = board_h - (h1 / 2) - (h1)*unit_square_pos_.row;
 	unit_obj_->pos_.x = (w1 / 2) + ((w1)*unit_square_pos_.col);
-
 	unit_obj_->Update(delta_time);
+
 }
