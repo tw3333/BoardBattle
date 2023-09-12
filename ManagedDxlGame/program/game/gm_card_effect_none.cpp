77bdd1fe17cefe96256211b2813f4e200c629026
hyou0064 <vthpjn@gmail.com>
2023-09-12 15:44:09 +0900

    喫煙コミット

---
+++ ManagedDxlGame/program/game/gm_card_effect_none.cpp
@@ -0,0 +1,14 @@
+#include "gm_card_effect_none.h"
+
+void CardEffectNone::Effect(std::vector<Unit*> units)
+{
+}
+
+bool CardEffectNone::CanEffectExcute(std::vector<SquarePos> range_square_pos, Board* board)
+{
+	return false;
+}
+
+void CardEffectNone::EffectExcute(std::vector<SquarePos> target_square_pos, Board* board)
+{
+}
---
+++ ManagedDxlGame/program/game/gm_card_effect_none.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "gm_card_effect.h"
+
+
+//DebugópÅAâΩÇ‡ãNÇ±Ç≥Ç»Ç¢
+
+
+class CardEffectNone : public CardEffect {
+public:
+
+	CardEffectNone(int ref_num) : CardEffect(ref_num) {}
+	~CardEffectNone() {}
+
+	void Effect(std::vector<Unit*> units) override;
+	bool CanEffectExcute(std::vector<SquarePos> range_square_pos, Board* board) override;
+	void EffectExcute(std::vector<SquarePos> target_square_pos, Board* board) override;
+
+
+
+
+private:
+
+
+
+
+
+};
