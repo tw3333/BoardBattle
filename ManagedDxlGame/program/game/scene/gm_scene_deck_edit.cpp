0538dcb0a718f0d2da626928f8431823d19b9cc7
hyou0064 <vthpjn@gmail.com>
2023-09-15 18:01:04 +0900

    クラス作成コミット

---
+++ ManagedDxlGame/program/game/scene/gm_scene_deck_edit.cpp
@@ -0,0 +1,19 @@
+#include "gm_scene_deck_edit.h"
+
+void SceneDeckEdit::Initialzie() {
+
+
+
+}
+
+void SceneDeckEdit::Update(float delta_time) {
+
+
+
+}
+
+void SceneDeckEdit::Render() {
+
+
+
+}
---
+++ ManagedDxlGame/program/game/scene/gm_scene_deck_edit.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "gm_scene_base.h"
+
+class SceneDeckEdit : public SceneBase {
+public:
+
+	SceneDeckEdit(){}
+	~SceneDeckEdit(){}
+
+	void Initialzie() override;
+	void Update(float delta_time) override;
+	void Render() override;
+
+
+private:
+
+	
+
+
+
+
+
+
+};
